ecc66785b79d1983e4aebe2176f50bb2fd56009e
brankosp <kaiquepaula619@gmail.com>
2025-10-14 22:09:33 -0300

    Update Aula_liscomp.py

--- aula_listacom/Aula_liscomp.py
+++ aula_listacom/Aula_liscomp.py
@@ -37,7 +37,29 @@ for p in turma:
 
 for p in turma:
     print(p[0])
-'''
+
 
 for p in turma:
     print(f'{p[0]} tem {p[1]} anos de idade')
+'''
+
+turma2=list()
+dados=list()
+totmaior = totmenor = 0
+
+for c in range(0,6):
+    dados.append(str(input("Nome:")).upper().strip())
+    dados.append(int(input("Idade:")))
+    turma2.append(dados[:])
+    dados.clear()
+print(turma2)
+
+for p in turma2:
+    if p[1]>= 18:
+        print(f'{p[0]} é maior de idade , ele tem {p[1]} anos de idade')
+        totmaior += 1
+    else:
+        print(f'{p[0]} é menor de idade , ele tem {p[1]}anos de idade')
+        totmenor += 1 
+
+print(f'Temos {totmaior} maiores de idade e {totmenor} menores de idade')
